97ba8d8c24cb4a3f098a0a4cf8d5551c5b0f5d36
Glitch (hello-webpage) <none>
2020-05-07 19:25:16 +0000

    🏅📠 Checkpoint ./script.js:15950011/723 ./index.html:15950011/1467 ./style.css:15950011/168

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+/* global createCanvas, loadImage, background, image, dvdImage */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function setup(){
+  createCanvas(800, 600)
+  // We only want to load the logo once.
+  dvdImage = loadImage("https://cdn.glitch.com/eaea72a4-ac6d-4777-b76e-f37d75959aa5%2Fdvd.jpeg?1515761833387")
+}
+
+function draw(){
+  background(220)
+  // Draw the logo at the new position.
+  image(dvdImage, 50, 50, 200, 150)
+}
