380e7b4a513757b3fdf725d48ffcb01663ad018c
Glitch (hello-webpage) <none>
2020-07-07 23:54:21 +0000

    👖🔔 Checkpoint ./script.js:18588164/1263

--- script.js
+++ script.js
@@ -1,55 +1,42 @@
+framerate = 60
 function setup() {
   createCanvas(1000, 400);
-  frameRate(60)
+  frameRate(framerate)
 
 }
 // 1 meter = 10 units
-let ball
 
-let x_acc = 0
-const y_acc = 98
 
-let x_cor = 500
-let y_cor = 200
 
-let x_vel = 50
-let y_vel = 0
+
 colors = ['red','green','blue','yellow','black','pink']
-let color = 'red'
-
-function draw() {
-  background(220);
-  ball = circle(x_cor,y_cor,20)
-  x_cor += x_vel/60
-  x_vel += x_acc/60
-  if (x_cor <= 0 || x_cor >= 1000) {
-    x_vel = -x_vel}
-  y_cor += y_vel/60
-  y_vel += y_acc/60
-  if (y_cor >= 400 || y_cor <= 0) {
-    y_vel = -y_vel
-    color = colors[Math.floor(Math.random() * colors.length)];
-    fill(color)
-  }
-  
-}
-
 positions = []
 
+
+function spawn(n) {
+  while (n != 0) {
+    
+  }
+}
+
+
 class Ball {
-  function constructor (x_cor,y_cor,x_vel,y_vel,x_acc) {
+  function constructor (x_cor,y_cor,x_vel,y_vel=0) {
     this.x_cor = x_cor
     this.y_cor = y_cor
     this.x_vel = x_vel
     this.y_vel = y_vel
-    this.x_acc = x_acc 
-    this.color = colors[Math.floor(Math.random() * colors.length)] }
+    this.x_acc = 0
+    this.y_acc = 98
+    this.color = colors[Math.floor(Math.random() * colors.length)]
+    //positions.push([this.x_cor,this.y_cor])
+    circle(this.x_cor,this.y_cor,20)
+  }
   
   function collisionCheck(){
     for(let i=0; i<positions.length; i++) {
       dist = ((positions[0] ** 2)- (this.x_cor**2) + (positions[1] ** 2)- (this.y_cor**2))
-      if dist == 0
-      
+      if (dist !==0 && dist < 10) this.x_vel = -this.x_vel  
     }
     
   }
@@ -59,6 +46,7 @@ class Ball {
     this.y_cor = this.y_cor + this.y_vel/framerate
     this.x_vel = this.x_vel + this.x_acc/framerate
     this.y_vel = this.y_vel + this.y_acc/framerate
+    positions.push([this.x_cor,this.y_cor])
     
     if (this.y_cor >= 400 || this.y_cor <= 0) {
     this.y_vel = -this.y_vel
@@ -74,4 +62,10 @@ class Ball {
   }
     
   }
-    
+
+ball = new Ball(100,200,100)
+function draw() {
+  background(220);
+  ball.simulate()
+  circle(this.x_cor,this.y_cor,20)
+}
