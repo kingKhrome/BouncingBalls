31d7504285e89ddb4e438ab1209e41423c932928
Glitch (hello-webpage) <none>
2020-07-08 00:56:19 +0000

    💷🗺 Checkpoint ./script.js:18588164/475

--- script.js
+++ script.js
@@ -18,14 +18,15 @@ class Ball {
     this.x_acc = 0
     this.y_acc = 98
     this.color = colors[Math.floor(Math.random() * colors.length)]
-    positions[this.name] = [x_cor,y_cor]
+    positions[this.name] = [this.x_cor,this.y_cor]
   }
   
     collisionCheck(){ //checks for collisions between balls --will add later
-    for(let i=0; i<positions.length; i++) {
-      let dist = ((positions[0] ** 2)- (this.x_cor**2) + (positions[1] ** 2)- (this.y_cor**2))
-      if (dist !==0 && dist < 10) this.x_vel = -this.x_vel  
-    }  
+      let vals = Object.values(positions)
+      for(let i=0; i<vals.length; i++) {
+       let dist = ((vals[0] ** 2)- (this.x_cor**2) + (vals[1] ** 2)- (this.y_cor**2))
+       if (dist !==0 && dist < 5) this.x_vel = -this.x_vel  
+    }
   } 
   
     simulate() {
@@ -33,7 +34,7 @@ class Ball {
     this.y_cor = this.y_cor + this.y_vel/framerate
     this.x_vel = this.x_vel + this.x_acc/framerate
     this.y_vel = this.y_vel + this.y_acc/framerate
-    positions[this.name] = [x_cor,y_cor]
+    positions[this.name] = [this.x_cor,this.y_cor]
     
     if (this.y_cor >= 400 || this.y_cor <= 0) {
     this.y_vel = -this.y_vel
@@ -50,16 +51,16 @@ class Ball {
   }
 
 
-let ball1 = new Ball(50,200,100)
-let ball2 = new Ball(150,200,-100)
-let ball3 = new Ball(250,200,100)
-let ball4 = new Ball(350,200,-100)
-let ball5 = new Ball(450,200,100)
-let ball6 = new Ball(550,200,-100)
-let ball7 = new Ball(650,200,100)
-let ball8 = new Ball(750,200,-100)
-let ball9 = new Ball(850,200,100)
-let ball10 = new Ball(950,200,-100)
+let ball1 = new Ball(50,200,`ball1`,100)
+let ball2 = new Ball(150,200,`ball2`,-100)
+let ball3 = new Ball(250,200,`ball3`,100)
+let ball4 = new Ball(350,200,`ball4`,-100)
+let ball5 = new Ball(450,200,`ball5`,100)
+let ball6 = new Ball(550,200,`ball6`,-100)
+let ball7 = new Ball(650,200,`ball7`,100)
+let ball8 = new Ball(750,200,`ball8`,-100)
+let ball9 = new Ball(850,200,`ball9`,100)
+let ball10 = new Ball(950,200,`ball10`,-100)
 
 function draw() {
   background(220)  
